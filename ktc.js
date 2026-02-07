4e520fe3c2ba0ff8a8b3d8f0f481f233ee7c2d79
engbinka <binuuu1212@gmail.com>
2026-02-07 03:29:30 +0300

    web2025c

---
+++ ktc.js
@@ -0,0 +1,15 @@
+function itusi(){
+    let c = document.getElementById("s").value;
+    document.getElementById("p").innerHTML = c;
+}
+
+
+function insert(){
+    let  c2 = document.getElementById("n1").value;
+    document.getElementById("nshow").innerHTML =  c2;
+}
+
+
+function pr(){
+    alert("welcome");
+}
